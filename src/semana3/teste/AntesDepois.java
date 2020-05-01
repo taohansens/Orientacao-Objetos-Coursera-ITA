4b9df7b06d624e2947299b2ef382f52c0827c736
Tao Hansen <tao.santos@somosicev.com>
2020-04-30 21:54:37 -0300

    Test JUnit

---
+++ src/semana3/teste/AntesDepois.java
@@ -0,0 +1,5 @@
+package semana3.teste;
+
+public class AntesDepois {
+
+}
---
+++ src/semana3/teste/AntesDepoisTest.java
@@ -0,0 +1,23 @@
+package semana3.teste;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AntesDepoisTest {
+
+	@Test
+	void test1() {
+		System.out.println("Teste1");
+	}
+	
+	@Test
+	void test2() {
+		System.out.println("Teste2");
+	}
+	
+	@Test
+	void test3() {
+		System.out.println("Teste3");
+	}
+}
