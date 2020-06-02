78020ecf12028a3093ba7dc831462ce6d04bf4a1
Tao Hansen <tao.santos@somosicev.com>
2020-06-01 23:09:37 -0300

    Created

---
+++ src/semana5/FormasDeTratamento/Informal.java
@@ -0,0 +1,8 @@
+package semana5.FormasDeTratamento;
+
+public class Informal implements FormatadorNome {
+    @Override
+    public String formatarNome(String nome, String sobrenome) {
+        return nome;
+    }
+}
---
+++ src/semana5/FormasDeTratamento/Respeitoso.java
@@ -0,0 +1,17 @@
+package semana5.FormasDeTratamento;
+
+public class Respeitoso implements FormatadorNome {
+    private boolean sexo; //True=Mulher e False=Homem;
+
+    public Respeitoso(boolean sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String formatarNome(String nome, String sobrenome) {
+        if (sexo)
+            return "Sra. " + nome + " " + sobrenome;
+        else
+            return "Sr. " + nome + " " + sobrenome;
+    }
+}
